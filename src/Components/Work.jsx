c5690693f2bcde4daacb903447fb89eb2e1cdb64
bhargav <karandave503@gmail.com>
2024-01-15 16:06:31 +0530

    add the image in web

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -1,7 +1,12 @@
 import "../assets/Style/Work.scss";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
- 
+import gym from "../../src/assets/images/gym-image.png";
+import portfolio from '../../src/assets/images/portfolio-image.png'
+import ai from '../../src/assets/images/ai-image.png'
+import ramaai from '../../src/assets/images/ramaai.png'
+import crypto from '../../src/assets/images/crypto.png'
+
 
 const projects = [
   {
@@ -10,61 +15,58 @@ const projects = [
     description:
       "I Created a simple Gym website using react, sass and i also use a AOS for animation",
     url: "https://gymkd.netlify.app/",
-    imgSrc:"https://github.com/karandave06/images/blob/main/gymimg.png?raw=true",
+    imgSrc: gym,
     id: 1,
   },
 
   {
     date: "25/04/2023",
     title: "SNAKE GAME",
-    description:
-      "Classic Snake Game Play and remember old day's.",
+    description: "Classic Snake Game Play and remember old day's.",
     url: "https://snakegamekd.netlify.app/",
-    imgSrc:  "https://www.friv5online.com/files/images/1e/1e020d80dd3ea57aa7aa12e4569596e6.png",
+    imgSrc:
+      "https://www.friv5online.com/files/images/1e/1e020d80dd3ea57aa7aa12e4569596e6.png",
     id: 2,
   },
 
   {
     date: "01/05/2023",
     title: "Port Folio",
-    description:
-      "This is my first portfolio that i created using react",
+    description: "This is my first portfolio that i created using react",
     url: "https://kd-portfoliyo.vercel.app/",
-    imgSrc:" https://github.com/karandave06/images/blob/main/Screenshot%20(58).png?raw=true",
+    imgSrc:
+      portfolio,
     id: 3,
   },
 
   {
     date: "10/05/2023",
-    title: "Video Hub",
-    description:
-      "This is my first project on Chakra UI",
-    url: "https://video-huub.netlify.app/",
-    imgSrc:"https://github.com/karandave06/images/blob/main/videohub.png?raw=true",
+    title: "Crypto tracking app",
+    description: "This is crypt",
+    url: "https://cryptoapp-h4c35o1tm-karandave06.vercel.app/",
+    imgSrc:
+      crypto,
     id: 4,
   },
   {
     date: "25/05/2023",
-    title: "Tailwind Web",
-    description:
-      "Power of React-Tailwind",
-    url: "https://karandave06.netlify.app/",
-    imgSrc:"https://github.com/karandave06/images/blob/main/tailwind.png?raw=true",
+    title: "Rama Ai",
+    description: "Image generating ai ",
+    url: "https://rama-ai.vercel.app/",
+    imgSrc:
+      ramaai,
     id: 5,
   },
 
   {
     date: "01/06/2023",
     title: "Gpt 3",
-    description:
-      "Ai is future of Humans",
+    description: "Beautiful ai website using tailwind css",
     url: "https://gpt3kd.netlify.app/   ",
-    imgSrc:"https://github.com/karandave06/images/blob/main/Screenshot%20(59).png?raw=true",
+    imgSrc:
+      ai,
     id: 6,
   },
-
-
-  // 
 ];
 
 const Work = () => {
@@ -84,12 +86,14 @@ const Work = () => {
           >
             {projects.map((i) => (
               <div key={i.id} className="workItem">
-              <img src={i.imgSrc} alt="image is hear" />
-              <aside>
-              <h3>{i.title}</h3>
-                <p>{i.description}</p>
-                <a href={i.url} target={"blank"}>Visit Now  </a>
-              </aside>
+                <img src={i.imgSrc} alt="image is hear" />
+                <aside>
+                  <h3>{i.title}</h3>
+                  <p>{i.description}</p>
+                  <a href={i.url} target={"blank"}>
+                    Visit Now{" "}
+                  </a>
+                </aside>
               </div>
             ))}
           </Carousel>
